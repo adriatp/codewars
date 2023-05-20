ac1f28499e24b12739ccc27a12826366e37cea23
adriatp <tarradas.adria@gmail.com>
2023-05-20 15:43:33 +0200

    Initialized project structure

---
+++ 52a78825cdfc2cfc87000005/index.js
@@ -0,0 +1,24 @@
+const calc = function (expression) {
+	
+	return 0;
+};
+
+var assert = require('assert');
+describe("Tests", () => {
+  it("test", () => {
+		const tests = [
+			['1+1', 2],
+			['1 - 1', 0],
+			['1* 1', 1],
+			['1 /1', 1],
+			['-123', -123],
+			['123', 123],
+			['2 /2+3 * 4.75- -6', 21.25],
+			['12* 123', 1476],
+			['2 / (2 + 3) * 4.33 - -6', 7.732],
+		];
+
+	for ( const [input,expected] of tests )
+		assert.strictEqual( calc(input), expected );
+	});
+});
