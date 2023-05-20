6d1164a894dcbb32d1b5ec6c644edfb27d56bfb8
adriatp <tarradas.adria@gmail.com>
2023-05-20 15:33:46 +0200

    Solved

--- 515bb423de843ea99400000a/index.js
+++ 515bb423de843ea99400000a/index.js
@@ -1,21 +1,30 @@
 class PaginationHelper {
 	constructor(collection, itemsPerPage) {
-	// The constructor takes in an array of items and a integer indicating how many
-	// items fit within a single page
+		// The constructor takes in an array of items and a integer indicating how many
+		// items fit within a single page
+		this.collection = collection;
+		this.itemsPerPage = itemsPerPage;
 	}
 	itemCount() {
-	// returns the number of items within the entire collection
+		// returns the number of items within the entire collection
+		return this.collection.length;
 	}
 	pageCount() {
-	// returns the number of pages
+		// returns the number of pages
+		return Math.ceil(this.itemCount() / this.itemsPerPage);
 	}
 	pageItemCount(pageIndex) {
-	// returns the number of items on the current page. page_index is zero based.
-	// this method should return -1 for pageIndex values that are out of range
+		// returns the number of items on the current page. page_index is zero based.
+		// this method should return -1 for pageIndex values that are out of range
+		if (pageIndex >= 0 && pageIndex <  this.pageCount() - 1) return this.itemsPerPage;
+		if (pageIndex >= 0 && pageIndex == this.pageCount() - 1) return this.itemCount() % this.itemsPerPage != 0 ? this.itemCount() % this.itemsPerPage : this.itemsPerPage;
+		return -1;
 	}
 	pageIndex(itemIndex) {
-	// determines what page an item is on. Zero based indexes
-	// this method should return -1 for itemIndex values that are out of range
+		// determines what page an item is on. Zero based indexes
+		// this method should return -1 for itemIndex values that are out of range
+		if (itemIndex < 0 || itemIndex >= this.itemCount()) return -1;
+		return Math.floor(itemIndex / this.itemsPerPage);
 	}
 }
 
