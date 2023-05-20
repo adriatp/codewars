62558195226f137c922480a54e96c51357066cea
adriatp <tarradas.adria@gmail.com>
2023-05-20 15:01:55 +0200

    Set default stats for Pagination exercise

---
+++ 515bb423de843ea99400000a/index.js
@@ -0,0 +1,58 @@
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+	// The constructor takes in an array of items and a integer indicating how many
+	// items fit within a single page
+	}
+	itemCount() {
+	// returns the number of items within the entire collection
+	}
+	pageCount() {
+	// returns the number of pages
+	}
+	pageItemCount(pageIndex) {
+	// returns the number of items on the current page. page_index is zero based.
+	// this method should return -1 for pageIndex values that are out of range
+	}
+	pageIndex(itemIndex) {
+	// determines what page an item is on. Zero based indexes
+	// this method should return -1 for itemIndex values that are out of range
+	}
+}
+
+describe("Tests suite", () => {
+	const { strictEqual } = require('chai').assert;
+
+	function doTest(instance, methodName, expected, ...args) {
+		const actual = instance[methodName](...args);
+		strictEqual(actual, expected, `for ${methodName}(${args.join(', ')})`);
+	}
+
+	it("sample test : 24 items with 10 per page", () => {
+		const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+		const helper = new PaginationHelper(collection, 10)
+
+		doTest(helper, 'pageCount', 3);
+		doTest(helper, 'itemCount', 24);
+
+		doTest(helper, 'pageItemCount', 10, 1);
+		doTest(helper, 'pageItemCount', 4, 2);
+		doTest(helper, 'pageItemCount', -1, 3);
+		doTest(helper, 'pageIndex', -1, 40);
+
+		doTest(helper, 'pageIndex', 2, 22);
+		doTest(helper, 'pageIndex', 0, 3);
+		doTest(helper, 'pageIndex', 0, 0);
+		doTest(helper, 'pageIndex', -1, -1);
+		doTest(helper, 'pageIndex', -1, -23);
+		doTest(helper, 'pageIndex', -1, -15);
+	});
+
+	it ('empty collection', () => {
+		const empty = new PaginationHelper([], 10);
+
+		doTest(empty, 'pageCount', 0);
+		doTest(empty, 'itemCount', 0);
+		doTest(empty, 'pageIndex', -1, 0);
+		doTest(empty, 'pageItemCount', -1, 0);
+	});
+});
